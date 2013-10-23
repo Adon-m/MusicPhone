f1aa6137984ced6d7e5e50676a6270956240fd25
somename <someemail>
2013-10-23 16:18:49 +0300

    besouro automatic message

--- src/commons/Recommender.java
+++ src/commons/Recommender.java
@@ -168,6 +168,12 @@ public class Recommender implements IRecommender {
 			}
 		}
 		
+		for(int i=0; i<list.size();++i){
+			if(list.get(i).getStartDate().compareTo(date)==0){
+				list.remove(i);
+			}
+		}
+		
 		
 		
 		
