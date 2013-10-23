749c7add10605120e1076049eb5684993142d2e2
somename <someemail>
2013-10-23 17:02:01 +0300

    besouro automatic message

--- src/commons/Recommender.java
+++ src/commons/Recommender.java
@@ -159,12 +159,13 @@ public class Recommender implements IRecommender {
 			
 			for(int i=0;i<list.size(); ++i)
 			{
-				if (list.get(i).getArtist()==artist){
-				list.remove(i);
-				if(dest.size()>0&& list.size()>0){
 				if(dest.get(j).getArtist()==list.get(i).getArtist()){
 					list.remove(i);
-				}}
+				}
+				if (list.get(i).getArtist()==artist){
+				list.remove(i);
+				
+				
 			}
 		}
 		
