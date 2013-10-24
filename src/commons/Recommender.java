acd62c24bf28dd6e9d05aa08e516003b1d09ecd5
somename <someemail>
2013-10-24 08:44:46 +0300

    besouro automatic message

--- src/commons/Recommender.java
+++ src/commons/Recommender.java
@@ -107,29 +107,21 @@ public class Recommender implements IRecommender {
 	public void AddToIten(List<Destination> destList, List<Comparisons> concerts, GeoPoint current){
 		//make sure the iten list is not empty
 		Destination dest =null;
-		int index=0;
-		int size= concerts.size();
+		int index= ClosestDistance(concerts.get(0).getStartDate(), current, concerts);
 		
-		while(size>0){
+		dest= new Destination(concerts.get(index));
+		destList.add(dest);
+		RemoveEntries(concerts, dest.getStartDate(), dest.getArtist().toString());
 		
-			
-				 
-			// because the RemoveEntries fuction deletes all entries before the date added to the itenerary
-			// the next available date is always at index 0 because it is ordered chronologically
-		if(concerts.size()>0){
-			index = ClosestDistance (concerts.get(0).getStartDate(), current,concerts);
+		while(concerts.size()>0){
+			dest=null;
+			index=ClosestDistance(concerts.get(0).getStartDate(), current, concerts);
 			dest= new Destination(concerts.get(index));
+			RemoveEntries(concerts, dest.getStartDate(), dest.getArtist().toString());
 			
-			destList.add(dest);
-			
-			
-			
-			RemoveEntries(concerts, destList.get(destList.size()-1).getStartDate(), destList.get(destList.size()-1).getArtist());
-			current = destList.get(destList.size()-1).getPosition();	
 		}
-			}
-		size= concerts.size();size--;
-	}
+		
+		}
 	
 		
 		
